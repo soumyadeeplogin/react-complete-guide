358bc2512e9593d3b3e889eacaf46cc39cdc6ff4
soumyadeeplogin <soumyadeeplogin@gmail.com>
2018-06-26 22:55:45 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -18,14 +18,21 @@ class App extends Component {
     this.setState({persons: persons});
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Tony Stark', age: 28},
-        { name: event.target.value, age: 18},
-        { name: 'Dr. Stephen Strange', age: 25}
-      ] 
+  nameChangedHandler = ( event, id ) => {
+
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id
     })
+    const person= {
+      ...this.state.persons[personIndex]
+    }
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons})
   }
 
   tooglePersonsHandler = (event) => {
@@ -51,7 +58,8 @@ class App extends Component {
               click={() => this.deletePersonHandler(index)}
               name={person.name} 
               age={person.age}
-              key={person.id} />  
+              key={person.id} 
+              changed={(event) => this.nameChangedHandler(event, person.id)}/>  
           })}
       </div>
       );
